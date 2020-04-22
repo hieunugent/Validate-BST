a720ba82ed29001999e878ad2802dca5615f52bd
Henry Hieu Nguyen <hieunugent@gmail.com>
2020-04-22 13:52:41 -0700

    update testcase

--- TestCase.java
+++ TestCase.java
@@ -4,6 +4,13 @@ import static org.junit.Assert.assertTrue;
 public class TestCase{
   @Test
   public void TestCase1(){
-     
+
+  }
+
+
+  public TestBST extends ValidateBST.BST{
+    public TestBST (int value){
+      super(value);
+    }
   }
 }
